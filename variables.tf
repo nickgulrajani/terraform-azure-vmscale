6d2863f5bc4e5d679a4749daed2949f70e919756
Nicholas Gulrajani <gulrajan@wwt.com>
2020-12-05 11:26:52 -0500

    WIP

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "location" {
- description = "The location where resources will be created"
+ default = "East US"
 }
 
 variable "tags" {
@@ -13,7 +13,7 @@ variable "tags" {
 
 variable "resource_group_name" {
  description = "The name of the resource group in which the resources will be created"
- default     = "myResourceGroup"
+ default     = "NicholasgullResourceGroup"
 }
 
 variable "application_port" {
